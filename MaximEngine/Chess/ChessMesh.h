93b3466d635b1f1e527ab449dbc4000ac4a11e7d
Maxim Thibodeau <thibodeau.maxim@courrier.uqam.ca>
2025-03-24 01:38:46 -0400

    Ajustement pour Linux

--- MaximEngine/Chess/ChessMesh.h
+++ MaximEngine/Chess/ChessMesh.h
@@ -12,9 +12,9 @@ class ChessMesh{
             Load3DModelFromObjFile( "Data/Models/chess_tour.obj", true, true, true, true, m_Tour, &stride );
             Load3DModelFromObjFile( "Data/Models/chess_cavalier.obj", true, true, true, true, m_Cavalier );
             Load3DModelFromObjFile( "Data/Models/chess_fou.obj", true, true, true, true, m_Fou );
-            Load3DModelFromObjFile( "Data/Models/chess_king.obj", true, true, true, true, m_Roi );
+            Load3DModelFromObjFile( "Data/Models/chess_King.obj", true, true, true, true, m_Roi );
             Load3DModelFromObjFile( "Data/Models/chess_queen.obj", true, true, true, true, m_Reine );
-            Load3DModelFromObjFile( "Data/Models/chess_Pion.obj", true, true, true, true, m_Pion );
+            Load3DModelFromObjFile( "Data/Models/chess_pion.obj", true, true, true, true, m_Pion );
             Load3DModelFromObjFile( "Data/Models/case.obj", true, true, true, false, m_Case );
         }
 
